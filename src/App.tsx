7a72e8396670010b62acdff80e8e7f3a22917402
gleison <gleisondeivison3@gmail.com>
2023-10-11 14:06:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import { GlobalCss } from './styles'
+
+function App() {
+  return (
+    <>
+      <GlobalCss />
+      <div>
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+import VectorImage from '../../assets/images/Vector.png'
+
+const HeaderBar = styled.header`
+  background-image: url(${VectorImage});
+`
