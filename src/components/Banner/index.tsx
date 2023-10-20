745aab0bc8366e37f49ee71114d7cfdc589b527d
gleison <gleisondeivison3@gmail.com>
2023-10-20 17:42:01 -0300

    Banner, logo e titulo feitos

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,12 +1,19 @@
-import { Imagem } from './styles'
+import { Imagem, Titulo } from './styles'
 
 import bannerImg from '../../assets/images/Vector.png'
+import logo from '../../assets/images/logo.png'
 
 const Banner = () => (
   <Imagem style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
-      <h2>Viva experiências gastronômicas no conforto da sua casa</h2>
+      <Titulo>
+        Viva experiências gastronômicas <br />
+        no conforto da sua casa
+      </Titulo>
     </div>
+    <a href="#">
+      <img src={logo} alt="Efood" />
+    </a>
   </Imagem>
 )
 //continuar do minuto 10:37
--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -6,4 +6,22 @@ export const Imagem = styled.div`
   display: block;
   background-repeat: no-repeat;
   background-size: cover;
+  padding-top: 260px;
+  padding-left: 416px;
+
+  img {
+    display: block;
+    margin-top: 40px;
+    margin-left: 336px;
+    margin-right: auto;
+    width: 125px;
+    position: fixed;
+    top: 0;
+  }
+`
+
+export const Titulo = styled.h2`
+  font-size: 36px;
+  font-weight: bold;
+  max-width: 539px;
 `
